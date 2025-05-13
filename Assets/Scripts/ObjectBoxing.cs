b341d3e7e95b3952d49713d18b0550c250130116
white.hong <white.hong@unity.cn>
2025-05-13 18:12:04 +0800

    上传装箱耗时测试

--- Assets/Scripts/ObjectBoxing.cs
+++ Assets/Scripts/ObjectBoxing.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityMap.Managers;
 
 public class ObjectBoxing : MonoBehaviour
 {
@@ -20,20 +23,43 @@ public class ObjectBoxing : MonoBehaviour
 
     public enum GetTypeEnum
     {
-        GetType,
+        GetDirectly,
+        GetByDynamicMethod,
     }
     public int timePerFrame = 1000;
     public TestPart testPart;
     public InputParam inputParam;
     public GetTypeEnum getTypeEnum;
     private TestData testData;
+
     private FieldInfo[] fieldInfos;
+    private delegate TValue FieldGetter<T, TValue>(ref T obj);
+    private static readonly ConcurrentDictionary<FieldInfo, Delegate> _fieldAccessors = new();
+    private static FieldGetter<T, object> CreateFieldGetter<T>(FieldInfo field)
+    {
+        var method = new DynamicMethod(
+            $"Get_{field.DeclaringType.Name}_{field.Name}",
+            typeof(object),
+            new[] { typeof(T).MakeByRefType() },
+            field.DeclaringType,
+            skipVisibility: true);
+
+        var il = method.GetILGenerator();
+        il.Emit(OpCodes.Ldarg_0);
+        il.Emit(OpCodes.Ldfld, field);
+        if (field.FieldType.IsValueType)
+            il.Emit(OpCodes.Box, field.FieldType);
+        il.Emit(OpCodes.Ret);
+
+        return (FieldGetter<T, object>)method.CreateDelegate(typeof(FieldGetter<T, object>));
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         Type t = testData.GetType();
-        fieldInfos = t.GetFields();
+        fieldInfos = t.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        Debug.Log("fieldInfos.Length: " + fieldInfos.Length);
     }
 
     // Update is called once per frame
@@ -65,8 +91,11 @@ public class ObjectBoxing : MonoBehaviour
     {
         switch (getTypeEnum)
         {
-            case GetTypeEnum.GetType:
+            case GetTypeEnum.GetDirectly:
                 TestGetDirectly();
+                break;
+            case GetTypeEnum.GetByDynamicMethod:
+                TestGetByDynamicMethod();
                 break;
             default:
                 break;
@@ -75,13 +104,28 @@ public class ObjectBoxing : MonoBehaviour
 
     /// <summary>
     /// Add Comment by White Hong - 2025-05-09 17:53:24
-    /// 完全没有gc
+    /// 循环1000次，每帧耗时约9ms
     /// <summary>
     private void TestGetDirectly()
     {
         for (int i = 0; i < fieldInfos.Length; ++i)
         {
-            G(fieldInfos[i], testData);
+            var v = fieldInfos[i].GetValue(testData);
+            // G(fieldInfos[i], testData);
+        }
+    }
+
+    /// <summary>
+    /// Add Comment by White Hong - 2025-05-09 19:26:50
+    /// 循环1000次，每帧耗时约2.5ms
+    /// <summary>
+    private void TestGetByDynamicMethod()
+    {
+        for (int i = 0; i < fieldInfos.Length; ++i)
+        {
+            // 获取或创建字段访问器
+            var getter = (FieldGetter<TestData, object>)_fieldAccessors.GetOrAdd(fieldInfos[i], f => CreateFieldGetter<TestData>(f));
+            var v = getter(ref testData);
         }
     }
 
